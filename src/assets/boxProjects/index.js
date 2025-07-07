abbc2a1befe89178e70cff841c4a3e2fbda0cd9c
Don Leonel <leonel.don@hotmail.es>
2025-07-07 17:21:46 -0300

    fix: falla de importacion

--- src/assets/boxProjects/index.js
+++ src/assets/boxProjects/index.js
@@ -1,13 +1,13 @@
 import boxProject from './boxProject.webp'
 import imgBackend from './imgBackend.webp'
 import nexoBirrero from './nexoBirrero.webp'
-import UrlShortenerFrontend from './UrlShortenerFrontend.webp'
+import urlShortenerFrontend from './urlShortenerFrontend.webp'
 import hidraApp from './hidraAppFront.webp'
 
 export {
     boxProject,
     imgBackend,
-    nexoBirrero,
-    UrlShortenerFrontend,
-    hidraApp
+    nexoBirrero, 
+    hidraApp,
+    urlShortenerFrontend
 }
